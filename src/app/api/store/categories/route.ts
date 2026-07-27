9e96f0d47f8df50172abe7d5b697b908c728e73d
PinkPigPlataform <pinkpigplataform@gmail.com>
2026-07-27 20:34:00 -0300

    feat: implement GET endpoint to retrieve active categories by locale

--- src/app/api/store/categories/route.ts
+++ src/app/api/store/categories/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { connectMongo } from '@/lib/mongodb'
 import CategoryModel from '@/lib/models/Category'
-
+export const dynamic = 'force-dynamic'
 export async function GET(request: Request) {
     try {
         await connectMongo()
